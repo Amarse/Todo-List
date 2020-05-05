4eb866e1b287f7f2cb12e173deaac47dfc7e6f73
Amarse <byicy08@gmail.com>
2020-05-05 16:48:28 +0900

    파일 수정

--- src/components/pageTemplate/PageTemplate.tsx
+++ src/components/pageTemplate/PageTemplate.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import classNames from 'classnames/bind';
 
-import styles from './PageTemplate-module.scss';
 
-const cx = classNames.bind(styles);
-interface Props {}
+interface Props { }
 
 const PageTemplate: React.FC<Props> = ({ children }) => {
     return (
-        <div className={cx('page-template')}>
-            <h1>TODO LIST</h1>
-            <div className={cx('content')}>{children}</div>
+        <div className="page-template">
+            <h1 className="page-template-title">TODO LIST</h1>
+            <div className="page-template-text">{children}</div>
+            <div></div>
         </div>
     );
 };
